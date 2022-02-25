bfab163083f6bd7e8b22323c47211ac30e2e7890
student <student@eda-bc-10.localdomain>
2022-02-25 14:05:09 +1300

    fir attempt auth0

--- server/auth0.js
+++ server/auth0.js
@@ -2,8 +2,8 @@ const jwt = require('express-jwt')
 const jwks = require('jwks-rsa')
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://kahu-2022-geoff.au.auth0.com'
-const audience = 'https://fruits/api'
+const domain = 'https://dev-jjh053qk.us.auth0.com'
+const audience = 'https://betterme/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
