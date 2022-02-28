c0bd782c15e8e988b086812ae40541c075773033
Evelyn Tan <evetan.wy@gmail.com>
2022-02-28 23:39:08 +1300

    homepage and mygoals page displaying and functionality working

--- client/components/HomeAuth.jsx
+++ client/components/HomeAuth.jsx
@@ -1,72 +1,78 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import SetGoals from './SetGoals'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import SetGoals from "./SetGoals";
 
-import { thunkAddNewGoal, thunkGetAllGoals } from '../actions/goals'
-import { useAuth0 } from '@auth0/auth0-react'
+import { thunkAddNewGoal, thunkGetAllGoals } from "../actions/goals";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function HomeAuth () {
-  const { isAuthenticated } = useAuth0()
-  const dispatch = useDispatch()
-  const results = useSelector((globalState) => globalState.goals)
-  const newGoals = useSelector((globalState) => globalState.newGoals)
-  const [todos, setTodos] = useState([])
-  const [status, setStatus] = useState('all')
-  const [inputText, setInputText] = useState('') // state for form to add new goal
-  const [filteredGoals, setFilteredGoals] = useState([])
+function HomeAuth() {
+  const { isAuthenticated } = useAuth0();
+  const dispatch = useDispatch();
+  const results = useSelector((globalState) => globalState.goals);
+  const newGoals = useSelector((globalState) => globalState.newGoals);
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [inputText, setInputText] = useState(""); // state for form to add new goal
+  const [filteredGoals, setFilteredGoals] = useState([]);
 
   // On load when the app runs
   useEffect(() => {
-    dispatch(thunkGetAllGoals())
-  }, [])
+    dispatch(thunkGetAllGoals());
+  }, []);
 
   useEffect(() => {
-    filterHandler()
-  }, [todos, status])
+    filterHandler();
+  }, [todos, status]);
 
   const inputTextHandler = (evt) => {
-    setInputText(evt.target.value)
-  }
+    setInputText(evt.target.value);
+  };
 
   const submitGoalHandler = (evt) => {
-    evt.preventDefault()
+    evt.preventDefault();
     setTodos(
       [
         ...todos,
         {
           details: inputText,
-          completed: false
-        }
+          completed: false,
+        },
       ],
       dispatch(thunkAddNewGoal(inputText))
-    )
-    setInputText('')
-  }
+    );
+    setInputText("");
+  };
 
   const filteredResults = results.filter((goal) => {
-    const id = goal.id
-    return newGoals.includes(id)
-  })
+    const id = goal.id;
+    return newGoals.includes(id);
+  });
 
   // function to keep completed task and display when selected
   const filterHandler = () => {
     switch (status) {
-      case 'completed':
-        setFilteredGoals(todos.filter((todo) => todo.completed === true))
-        break
-      case 'uncompleted':
-        setFilteredGoals(todos.filter((todo) => todo.uncompleted === false))
-        break
+      case "completed":
+        setFilteredGoals(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredGoals(todos.filter((todo) => todo.uncompleted === false));
+        break;
       default:
-        setFilteredGoals(todos)
+        setFilteredGoals(todos);
     }
-  }
+  };
 
   return (
     isAuthenticated && (
       <>
-        <form>
-          <input onChange={inputTextHandler} value={inputText} className="goalInput-text-box" type="text" placeholder="Enter your goal here..."></input>
+        <form onSubmit={submitGoalHandler}>
+          <input
+            onChange={inputTextHandler}
+            value={inputText}
+            className="goalInput-text-box"
+            type="text"
+            placeholder="Enter your goal here..."
+          ></input>
         </form>
 
         <div className="goals-card">
@@ -88,14 +94,18 @@ function HomeAuth () {
           </div>
 
           <div>
-            <button onClick={submitGoalHandler} type="submit" className="goals-card-button">
-            Submit
+            <button
+              onClick={submitGoalHandler}
+              type="submit"
+              className="goals-card-button"
+            >
+              Submit
             </button>
           </div>
         </div>
       </>
     )
-  )
+  );
 }
 
-export default HomeAuth
+export default HomeAuth;
--- client/components/MyGoals.jsx
+++ client/components/MyGoals.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import SetGoals from "./SetGoals";
 
-import { useAuth0 } from '@auth0/auth0-react'
+import { useAuth0 } from "@auth0/auth0-react";
+import { thunkGetAllGoals } from "../actions/goals";
 
-const MyGoals = () => {
-  const { isAuthenticated } = useAuth0()
+const MyGoals = ({ todos, setTodos, filteredGoals }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(thunkGetAllGoals());
+  }, []);
 
+  const { isAuthenticated } = useAuth0();
+  const newGoals = useSelector((globalState) => globalState.newGoals);
+  const results = useSelector((globalState) => globalState.goals);
+
+  const filteredResults = results.filter((goal) => {
+    const id = goal.id;
+
+    return newGoals.includes(id);
+  });
   return (
     isAuthenticated && (
       <>
-
         <div className="mygoals-card">
-
-          <h3><u>My Goals</u></h3>
+          <h3>
+            <u>My Goals</u>
+          </h3>
 
           {/* To display goals */}
-          <div></div>
-
+          <div>
+            <ul>
+              {results.map((goals) => (
+                <li>
+                  {goals.details} {goals.completed}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-
       </>
     )
-  )
-}
+  );
+};
 
-export default MyGoals
+export default MyGoals;
